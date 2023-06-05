1633aac282f1bccb35ae6cd58f160c93dd0edbf8
Abdullah Yasykur BM <114162816+FadhlyABD@users.noreply.github.com>
2023-06-05 22:03:36 +0700

    Add files via upload

---
+++ hotel/main/footer.php
@@ -0,0 +1,65 @@
+<!-- Footer -->
+
+<h6 class="text-center p-2 mt-2 h-font custom-bg-2">Designed and Developed by Mabuk Basis Data</h6>
+
+<div class="container-fluid custom-bg-4 sm-center">
+    <div class="row">
+        <div class="col-lg-3 p-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Rendezvouz Surabaya</h5>
+            <p class="sm-text s-font text-color">Adress: Surabaya, Kedurus, Kec. Karangpilang, Surabaya, Jawa Timur 60223</p>
+            <p class="sm-text s-font text-color">Phone: 0813 3619 7912</p>
+            <p class="sm-text s-font text-color">Email: surabaya@rendezvouz.com</p>
+        </div>
+        <div class="col-lg-3 p-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Check-in / Check-out</h5>
+            <p class="sm-text s-font text-color mb-1-">We hope you’ve enjoyed your stay from start to finish. Please note the times below:</p>
+            <p class="sm-text s-font text-color m-0">Check-in: 2pm</p>
+            <p class="sm-text s-font text-color">Check-out: 12noon</p>
+        </div>
+        <div class="col-lg-2 p-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Overview</h5>
+            <div class="row">
+                <div class="col-lg-6">
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">About</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Rooms & Suites</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Dining</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Events</a><br>
+                </div>
+                <div class="col-lg-6">
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Gallery</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Offers</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">More</a>
+                </div>
+            </div>                
+        </div>
+        <div class="col-lg-2 ps-lg-4 pt-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Follow Us</h5>
+            <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color"> 
+                <i class="bi bi-twitter me-1"></i> Twitter
+            </a><br>
+            <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color"> 
+                <i class="bi bi-facebook me-1"></i> Facebook
+            </a><br>
+            <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color"> 
+                <i class="bi bi-instagram me-1"></i> Instagram
+            </a><br>
+            <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color"> 
+                <i class="bi bi-tiktok me-1"></i> Tiktok
+            </a><br>
+        </div>
+        <div class="col-lg-2 pe-lg-4 pt-4">
+            <h5 class="mb-4 fw-medium s-font text-color">Payment Methods</h5>
+            <p class="sm-text s-font text-color">Payment we accept:</p>
+            <p class="sm-text s-font text-color">VISA, QRIS, MASTER CARDS, ETC</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid custom-bg-2 text-center pt-3">
+    <a class="navbar-brand s-font fs-3" href="index.php">
+        <img src="/hotel/images/about/hotelLogo.svg" alt="Logo" width="60" height="53" class="d-inline-block align-text-center">  
+        Rendezvouz
+    </a>
+    <h6 class="s-font fw-medium">Privacy Policy | Terms & Conditions | Disclaimer | Supplier Code Of Conduct</h6>
+    <h6 class="sm-text s-font">© 2023 Rendezvouz International Hotel Management Ltd. All Rights Reserved. ICP license: 17055189</h6>
+</div>
