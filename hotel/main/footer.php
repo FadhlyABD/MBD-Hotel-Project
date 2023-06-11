70ad948e91840e8be70f09497bed42bf6869b7e9
Abdullah Yasykur BM <114162816+FadhlyABD@users.noreply.github.com>
2023-06-11 13:33:17 +0700

    Add files via upload

---
+++ hotel/main/footer.php
@@ -0,0 +1,69 @@
+<!-- Footer -->
+
+<h6 class="text-center p-2 mt-2 h-font custom-bg-2">Designed and Developed by Mabuk Basis Data</h6>
+
+<div class="container-fluid custom-bg-4 sm-center">
+    <div class="row">
+        <div class="col-lg-3 p-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Rendezvouz Surabaya</h5>
+            <p class="sm-text s-font text-color">Adress: Surabaya, Kedurus, Kec. Karangpilang, Surabaya, Jawa Timur 60223</p>
+            <p class="sm-text s-font text-color">Phone: 0813 3619 7912</p>
+            <p class="sm-text s-font text-color">Email: surabaya@rendezvouz.com</p>
+        </div>
+        <div class="col-lg-3 p-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Check-in / Check-out</h5>
+            <p class="sm-text s-font text-color mb-1-">We hope you’ve enjoyed your stay from start to finish. Please note the times below:</p>
+            <p class="sm-text s-font text-color m-0">Check-in: 2pm</p>
+            <p class="sm-text s-font text-color">Check-out: 12noon</p>
+        </div>
+        <div class="col-lg-2 p-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Overview</h5>
+            <div class="row">
+                <div class="col-lg-6">
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">About</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Rooms & Suites</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Dining</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Events</a><br>
+                </div>
+                <div class="col-lg-6">
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Gallery</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">Offers</a><br>
+                    <a href="#" class="d-inline-block mb-1 text-decoration-none sm-text s-font text-color">More</a>
+                </div>
+            </div>                
+        </div>
+        <div class="col-lg-2 ps-lg-4 pt-4">
+            <h5 class="mb-3 fw-medium s-font text-color">Follow Us</h5>
+            <a href="#" class="d-inline-block mb-2 text-decoration-none sm-text s-font text-color"> 
+                <img src="/hotel/images/about/Twitter2.svg" alt="" width="20" height="15" class="d-inline-block align-text-center"> 
+                Twitter
+            </a><br>
+            <a href="https://www.facebook.com/vano.vano.731?mibextid=ZbWKwL" class="d-inline-block mb-2 text-decoration-none sm-text s-font text-color"> 
+                <img src="/hotel/images/about/Facebook2.svg" alt="" width="20" height="15" class="d-inline-block align-text-center"> 
+                Facebook
+            </a><br>
+            <a href="https://instagram.com/duevano.fairuz?igshid=MzRlODBiNWFlZA==" class="d-inline-block mb-2 text-decoration-none sm-text s-font text-color"> 
+                <img src="/hotel/images/about/Instagram2.svg" alt="" width="20" height="15" class="d-inline-block align-text-center"> 
+                Instagram
+            </a><br>
+            <a href="https://www.tiktok.com/@duevano_f?_t=8cweI5RPFb8&_r=1" class="d-inline-block mb-2 text-decoration-none sm-text s-font text-color"> 
+                <img src="/hotel/images/about/Tiktok2.svg" alt="" width="20" height="15" class="d-inline-block align-text-center"> 
+                Tiktok
+            </a><br>
+        </div>
+        <div class="col-lg-2 pe-lg-4 pt-4">
+            <h5 class="mb-4 fw-medium s-font text-color">Payment Methods</h5>
+            <p class="sm-text s-font text-color">Payment we accept:</p>
+            <p class="sm-text s-font text-color">VISA, QRIS, MASTER CARDS, ETC</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid custom-bg-2 text-center pt-3">
+    <a class="navbar-brand s-font fs-3" href="index.php">
+        <img src="/hotel/images/about/hotelLogo.svg" alt="Logo" width="60" height="53" class="d-inline-block align-text-center">  
+        Rendezvouz
+    </a>
+    <h6 class="s-font fw-medium">Privacy Policy | Terms & Conditions | Disclaimer | Supplier Code Of Conduct</h6>
+    <h6 class="sm-text s-font">© 2023 Rendezvouz International Hotel Management Ltd. All Rights Reserved. ICP license: 17055189</h6>
+</div>
---
+++ hotel/main/header.php
@@ -0,0 +1,191 @@
+<!-- Navbar -->
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="d-flex justify-content-between mx-auto p-2">
+    <a class="navbar-brand s-font" href="index.php">
+        <img src="/hotel/images/about/hotelLogo.svg" alt="Logo" width="50" height="44" class="d-inline-block align-text-center">  
+        Rendezvouz
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon sm-center"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li> -->
+        <li class="nav-item">
+        <a class="nav-link s-font fw-medium" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">
+            <img src="/hotel/images/about/signinIcon.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            Sign In</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link s-font fw-medium" href="#">Join Now</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link s-font fw-medium" href="#">Find Reservations</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="/hotel/images/about/language.svg" alt="Logo" width="20" height="24" class="d-inline-block align-text-top">
+            English
+        </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Indonesia</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Arabic</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            IDR
+        </a>
+        <ul class="dropdown-menu no-radius no-border">
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">IDR </span> Indonesian Rupiah</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">MYR </span> Malaysian Ringgit</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">USD </span> U.S. Dollar</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">EUR </span> Euro</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">SGD </span> Singapore Dollar</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">JPY </span> Japanese Yen</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">SAR </span> Saudi Arabian Riyal</a></li>
+        </ul>
+        </li>
+    </ul>
+    </div>
+</div>
+</nav>
+<snav class="navbar navbar-expand-lg navbar custom-bg-4 border-bottom border-bottom-dark" data-bs-theme="dark">
+<div class="d-flex justify-content-evenly mx-auto">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto my-0 mb-lg-0">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            ABOUT
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark sm-text s-font" href="#">Action</a></li>
+            <li><a class="dropdown-item text-dark sm-text s-font" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark sm-text s-font" href="#">Something else here</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            ROOMS & SUITES
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius dropdown-multicol">
+            <div class="dropdown-col">
+                <li><h6 class="dropdown-header h-font text-dark fw-bold">Rooms</h6></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Action</a></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+            </div>
+            <div class="dropdown-col">
+                <li><h6 class="dropdown-header h-font text-dark fw-bold">Suites</h6></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Action</a></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+            </div>
+            <div class="dropdown-col">
+                <li><h6 class="dropdown-header h-font text-dark fw-bold">Connecting Rooms</h6></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Action</a></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+            </div>
+            <li><a class="dropdown-item text-dark" href="rooms.php">ROOMS & SUITES OVERVIEW</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            DINING
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Action</a></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">DINING OVERVIEW</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            EVENTS
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Action</a></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Something else here</a></li>
+        </ul>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link s-font fw-medium text-color" href="#">GALLERY</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link s-font fw-medium text-color" href="#">OFFERS</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            MORE
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark" href="#">Action</a></li>
+            <li><a class="dropdown-item text-dark" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark" href="#">Something else here</a></li>
+        </ul>
+        </li>
+    </ul>
+    <form class="d-flex" role="search">
+        <input class="form-control me-2 no-radius" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn text-color s-font fw-medium bg-dark" type="submit">Search</button>
+    </form>
+    </div>
+</div>
+</snav>
+
+<!-- Modal -->
+
+<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header p-2">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <p class="modal-title fs-2 h-font text-center" id="staticBackdropLabel">Sign In</p>                            
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <label for="inputPassword5" class="form-label">Password</label>
+                    <input type="password" id="inputPassword5" class="form-control" aria-labelledby="passwordHelpBlock">
+                    <div id="passwordHelpBlock mb-3" class="form-text">
+                        Your password must be 8-20 characters long.
+                    </div>
+                    <hr>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </div>
+                <hr>
+                <div class="d-grid gap-2">
+                    <button class="btn custom-bg-1" type="button">Sign In</button>                     
+                </div>
+            </div>
+            <div class="modal-footer">
+                
+            </div>
+        </div>
+    </div>
+</div>
