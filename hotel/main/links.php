b7f44f1704b782e62262871dd880088ee690f717
Achmad Khosyi' A. R <114477225+Khosyi0@users.noreply.github.com>
2023-06-12 23:08:22 +0700

    Update links.php

--- hotel/main/links.php
+++ hotel/main/links.php
@@ -1,3 +1,10 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Cardo:wght@400;700&family=Montserrat:wght@400;500;700&family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="css/hcommon.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+<?php
+    //gtw buat apa, sementara tambahin
+    session_start();
+
+?>
