ba90dc953a6857ffa60561ff8eb37a1f5f5264aa
Duevano Fairuz Pandya <114162555+duevanofairuz@users.noreply.github.com>
2023-06-13 13:59:06 +0700

    Add files via upload

---
+++ hotel/dining.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dining | Rendezvous Hotel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <?php require('main/links.php'); ?>
+</head>
+
+<body class="custom-bg-5">
+    <?php require('main/header.php'); ?>
+
+    <!-- navigator start -->
+    <div class="custom-margin py-2 text-left">
+        <ul class="navbar-nav d-flex list-group list-group-horizontal-sm">
+            <li class="nav-item"><a href="index.php" class="nav-link s-font fw-medium text-color2 md-text">Hotel ></a>
+            </li>
+            <li class="nav-item"><a href="#" class="nav-link s-font fw-medium md-text">&nbsp;Dining</a></li>
+        </ul>
+    </div>
+    <!-- navigator end -->
+
+    <!-- swiper start -->
+    <div class="container-fluid px-0">
+        <h1 class="mt-5 h-font carousel-text text-color">Rendezvous Special Dining</h1>
+        <div class="swiper swiper-container-1">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="/hotel/images/dining/diningswiper1.svg" class="w-100 d-block">
+                </div>
+                <div class="swiper-slide">
+                    <img src="/hotel/images/dining/diningswiper2.svg" class="w-100 d-block">
+                </div>
+                <div class="swiper-slide">
+                    <img src="/hotel/images/dining/diningswiper3.svg" class="w-100 d-block">
+                </div>
+                <div class="swiper-slide">
+                    <img src="/hotel/images/dining/diningswiper4.svg" class="w-100 d-block">
+                </div>
+            </div>
+            <!-- <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div> -->
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+    <!-- swiper end -->
+
+    <!-- title start -->
+    <div class="custom-margin margintop-20">
+        <h4 class="mt-5 h-font text-center">A Myriad of Delectable Tastes for food connoisseurs</h4>
+        <h6 class="margintop-20 mx-5 md-text text-center">Featuring 6 food and beverage outlets, Rendezvous,
+            Surabaya offers a wide array of culinary experiences under one roof. With several international chefs on
+            board, Rendezvous, Surabaya is home to a rich variety of tantalising global cuisines. </h6>
+        <h6 class="margintop-20 mx-5 md-text text-center"> In addition, choices of wellness cuisine are also available
+            to enable guests to continue their healthy eating routines, and stay well even when you are away from home.
+        </h6>
+    </div>
+    <!-- title end -->
+
+    <!-- listroom rev start -->
+    <div class="container mx-lg-8 px-lg-8">
+        <div class="row m-5 px-lg-5">
+            <div class="col-md-4 mb-3">
+                <div class="card m-2 no-radius text-center bg-transparent">
+                    <img src="/hotel/images/dining/resto1.svg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="mt-2">Joder</h6>
+                        <p class="card-text sm-text">Rendezvous Surabaya<br>Surabaya<br>International | $</p>
+                        <a href="#" class="btn custom-bg-1 no-radius text-white sm-text mb-2">View Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card m-2 no-radius text-center bg-transparent">
+                    <img src="/hotel/images/dining/resto2.svg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="mt-2">Mixue</h6>
+                        <p class="card-text sm-text">Rendezvous Surabaya<br>Surabaya<br>International | $</p>
+                        <a href="#" class="btn custom-bg-1 no-radius text-white sm-text mb-2">View Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card m-2 no-radius text-center bg-transparent">
+                    <img src="/hotel/images/dining/resto3.svg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="mt-2">Domino's Pizza</h6>
+                        <p class="card-text sm-text">Rendezvous Surabaya<br>Surabaya<br>International | $$$</p>
+                        <a href="#" class="btn custom-bg-1 no-radius text-white sm-text mb-2">View Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card m-2 no-radius text-center bg-transparent">
+                    <img src="/hotel/images/dining/resto4.svg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="mt-2">Dinasti</h6>
+                        <p class="card-text sm-text">Rendezvous Surabaya<br>Surabaya<br>International | $</p>
+                        <a href="#" class="btn custom-bg-1 no-radius text-white sm-text mb-2">View Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card m-2 no-radius text-center bg-transparent">
+                    <img src="/hotel/images/dining/resto5.svg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="mt-2">Warju</h6>
+                        <p class="card-text sm-text">Rendezvous Surabaya<br>Surabaya<br>International | $</p>
+                        <a href="#" class="btn custom-bg-1 no-radius text-white sm-text mb-2">View Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card m-2 no-radius text-center bg-transparent">
+                    <img src="/hotel/images/dining/resto6.svg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="mt-2">Gacoan</h6>
+                        <p class="card-text sm-text">Rendezvous Surabaya<br>Surabaya<br>International | $</p>
+                        <a href="#" class="btn custom-bg-1 no-radius text-white sm-text mb-2">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <?php require('main/footer.php'); ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <script>
+        var swiper = new Swiper(".swiper-container-1", {
+            spaceBetween: 0,
+            loop: true,
+            centeredSlides: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            // navigation: {
+            //     nextEl: ".swiper-button-next",
+            //     prevEl: ".swiper-button-prev",
+            // },
+        });
+
+        var swiper = new Swiper(".swiper-container-2", {
+            spaceBetween: 30,
+            loop: true,
+            centeredSlides: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            // navigation: {
+            //     nextEl: ".swiper-button-next",
+            //     prevEl: ".swiper-button-prev",
+            // },
+        });
+    </script>
+
+</body>
+
+</html>
