9234780e430ec1b666fbf62d7010f03da44751df
Achmad Khosyi' A. R <114477225+Khosyi0@users.noreply.github.com>
2023-06-13 18:16:54 +0700

    Update logout.php

--- hotel/logout.php
+++ hotel/logout.php
@@ -1,7 +1,17 @@
 <?php
     require('admin/main/essentials.php');
+    require('admin/main/db_config.php');
 
     session_start();
+
+    // Mendapatkan u_id dari session
+    $u_id = $_SESSION['uId'];
+
+    // Update status menjadi 0
+    $update_query = "UPDATE `user` SET `status` = 0 WHERE `u_id` = ?";
+    $update_values = [$u_id];
+    update($update_query, $update_values, 'i');
+
     session_destroy();
     redirect('index.php');
 ?>
