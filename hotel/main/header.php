4c7e7510f22df0d92b4e7cc9600d1b5e04a37575
Abdullah Yasykur BM <114162816+FadhlyABD@users.noreply.github.com>
2023-06-14 07:14:21 +0700

    Add files via upload

---
+++ hotel/main/header.php
@@ -0,0 +1,261 @@
+<!-- Navbar -->
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="d-flex justify-content-between mx-auto p-2">
+    <a class="navbar-brand s-font" href="index.php">
+        <img src="/hotel/images/about/hotelLogo.svg" alt="Logo" width="50" height="44" class="d-inline-block align-text-center">  
+        Rendezvous
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon sm-center"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li> -->
+        <?php
+            if(isset($_SESSION['login']) && $_SESSION['login'] == true)
+            {
+                echo<<<data
+                <li class="nav-item">
+                <a class="nav-link s-font fw-medium" href="#" data-bs-toggle="modal">
+                <i class="bi bi-person-check"></i>
+                    $_SESSION[uName]</a>    
+                </li>
+                <li class="nav-item">
+                <a class="nav-link s-font fw-medium" href="logout.php">
+                    <i class="bi bi-box-arrow-left"></i>
+                    Log Out</a>
+                </li>
+                data;
+            }
+            else{
+                echo<<<data
+                <li class="nav-item">
+                <a class="nav-link s-font fw-medium" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">
+                <i class="bi bi-person"></i>
+                    Log In</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link s-font fw-medium" href="#" data-bs-toggle="modal" data-bs-target="#registerModal">
+                <i class="bi bi-person"></i>
+                    Register</a>
+                </li>
+                data;
+            }
+            
+            
+        ?>
+        
+        <li class="nav-item">
+        <a class="nav-link s-font fw-medium" href="#">Find Reservations</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-globe"></i>
+            English
+        </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Indonesia</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Arabic</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            IDR
+        </a>
+        <ul class="dropdown-menu no-radius no-border">
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">IDR </span> Indonesian Rupiah</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">MYR </span> Malaysian Ringgit</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">USD </span> U.S. Dollar</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">EUR </span> Euro</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">SGD </span> Singapore Dollar</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">JPY </span> Japanese Yen</a></li>
+            <li><a class="dropdown-item" href="#"><span class="font-monospace fw-bold">SAR </span> Saudi Arabian Riyal</a></li>
+        </ul>
+        </li>
+    </ul>
+    </div>
+</div>
+</nav>
+<snav class="navbar navbar-expand-lg navbar custom-bg-4 border-bottom border-bottom-dark" data-bs-theme="dark">
+<div class="d-flex justify-content-evenly mx-auto">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto my-0 mb-lg-0">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            ABOUT
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark sm-text s-font" href="aboutpage.php">Action</a></li>
+            <li><a class="dropdown-item text-dark sm-text s-font" href="aboutpage.php">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark sm-text s-font" href="aboutpage.php">Something else here</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            ROOMS & SUITES
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius dropdown-multicol">
+            <div class="dropdown-col">
+                <li><h6 class="dropdown-header h-font text-dark fw-bold">Rooms</h6></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Action</a></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+            </div>
+            <div class="dropdown-col">
+                <li><h6 class="dropdown-header h-font text-dark fw-bold">Suites</h6></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Action</a></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+            </div>
+            <div class="dropdown-col">
+                <li><h6 class="dropdown-header h-font text-dark fw-bold">Connecting Rooms</h6></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Action</a></li>
+                <li><a class="dropdown-item text-dark md-text s-font" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+            </div>
+            <li><a class="dropdown-item text-dark" href="rooms.php">ROOMS & SUITES OVERVIEW</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            DINING
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Action</a></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark md-text" href="dining.php">DINING OVERVIEW</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            FACILITIES
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Action</a></li>
+            <li><a class="dropdown-item text-dark md-text" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark md-text" href="facilities.php">FACILITIES OVERVIEW</a></li>
+        </ul>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link s-font fw-medium text-color" href="https://www.youtube.com/@WindahBasudara">GALLERY</a>
+        </li>
+        <!-- <li class="nav-item">
+        <a class="nav-link s-font fw-medium text-color" href="#">OFFERS</a>
+        </li> -->
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle s-font fw-medium text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            MORE
+        </a>
+        <ul class="dropdown-menu custom-bg-2 no-radius">
+            <li><h6 class="dropdown-header">Dropdown header</h6></li>
+            <li><a class="dropdown-item text-dark" href="#">Action</a></li>
+            <li><a class="dropdown-item text-dark" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item text-dark" href="https://youtu.be/rePXVSWwNIY">Something else here</a></li>
+        </ul>
+        </li>
+    </ul>
+    <form class="d-flex" role="search">
+        <input class="form-control me-2 no-radius" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn text-color s-font fw-medium bg-dark" type="submit">Search</button>
+    </form>
+    </div>
+</div>
+</snav>
+
+<!-- Modal -->
+
+<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <form id="login_form">
+                <div class="modal-header p-2">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <p class="modal-title fs-2 h-font text-center" >User Log In</p>                            
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Username</label>
+                            <input name="name" type="text" class="form-control" placeholder="Username" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input name="pass" type="password" class="form-control" aria-labelledby="passwordHelpBlock" required>
+                            <div id="passwordHelpBlock mb-3" class="form-text">
+                                Your password must be 8-20 characters long.
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn custom-bg-1" type="button">Log In</button>                     
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <form id="register_form">
+                <div class="modal-header p-2">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <p class="modal-title fs-2 h-font text-center">Register</p>                            
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Username</label>
+                            <input name="name" type="text" class="form-control" placeholder="Username" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Email address</label>
+                            <input name="email" type="email" class="form-control" placeholder="name@example.com" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input name="pass" type="password" class="form-control" aria-labelledby="passwordHelpBlock" required>
+                            <div id="passwordHelpBlock mb-3" class="form-text">
+                                Your password must be 8-20 characters long.
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Confirm Password</label>
+                            <input name="cpass" type="password" class="form-control" required>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn custom-bg-1" type="button">Register</button>                     
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
